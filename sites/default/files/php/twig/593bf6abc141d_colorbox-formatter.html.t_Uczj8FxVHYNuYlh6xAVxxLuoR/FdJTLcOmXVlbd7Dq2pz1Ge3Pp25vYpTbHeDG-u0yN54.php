c4dbb1ead05ff513bcfe71be1c83fa593957ff6a
Marc Isaacson <marc@isaacsonwebdevelopment.com>
2017-06-10 07:44:41 -0600

    Upgrade to Drupal 8.3.3

---
+++ sites/default/files/php/twig/593bf6abc141d_colorbox-formatter.html.t_Uczj8FxVHYNuYlh6xAVxxLuoR/FdJTLcOmXVlbd7Dq2pz1Ge3Pp25vYpTbHeDG-u0yN54.php
@@ -0,0 +1,88 @@
+<?php
+
+/* modules/contrib/colorbox/templates/colorbox-formatter.html.twig */
+class __TwigTemplate_2621821568ef5c74c52320a07f43af8499ae3a66c344381ab34cd2b5c91fcdea extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 16
+        echo "<a href=\"";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["url"]) ? $context["url"] : null), "html", null, true));
+        echo "\"";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["attributes"]) ? $context["attributes"] : null), "html", null, true));
+        echo ">";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["image"]) ? $context["image"] : null), "html", null, true));
+        echo "</a>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/contrib/colorbox/templates/colorbox-formatter.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 16,);
+    }
+
+    public function getSource()
+    {
+        return "{#
+/**
+ * @file
+ * Default theme implementation to display a formatted colorbox image field.
+ *
+ * Available variables:
+ * - image: A collection of image data.
+ * - url: An URL the image can be linked to.
+ * - attributes: Link attributes.
+ *
+ * @see template_preprocess_colorbox_formatter()
+ *
+ * @ingroup themeable
+ */
+#}
+<a href=\"{{ url }}\"{{ attributes }}>{{ image }}</a>
+";
+    }
+}
