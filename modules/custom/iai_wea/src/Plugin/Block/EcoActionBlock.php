87ca9d1b7fdd71e75ebeb262d89950cd9177bdff
Marc Isaacson <marc@isaacsonwebdevelopment.com>
2017-06-17 20:06:31 -0600

    Add the personalization service to the block

--- modules/custom/iai_wea/src/Plugin/Block/EcoActionBlock.php
+++ modules/custom/iai_wea/src/Plugin/Block/EcoActionBlock.php
@@ -3,11 +3,13 @@
 namespace Drupal\iai_wea\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Entity\EntityRepositoryInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Link;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Url;
+use Drupal\iai_personalization\PersonalizationIpServiceInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -30,6 +32,30 @@ class EcoActionBlock extends BlockBase implements ContainerFactoryPluginInterfac
 
 /******************************************************************************
  **                                                                          **
+ ** We are going to use the Entity Repository to get the correct translation **
+ ** of the Aquifer. We know that we have only English language versions on   **
+ ** our site. However, we should always write our code to be ready for a     **
+ ** multi-lingual site. We can't assume that the code will be running on a   **
+ ** site with only one language. Thus, we will take the necessary steps in   **
+ ** our code even before we get to the Multi-lingual section of the course.  **
+ **                                                                          **
+ ******************************************************************************/
+  /**
+   * The entity repository.
+   *
+   * @var \Drupal\Core\Entity\EntityRepositoryInterface
+   */
+  protected $entityRepository;
+
+  /**
+   * The personalization Ip Service.
+   *
+   * @var \Drupal\iai_personalization\PersonalizationIpServiceInterface
+   */
+  protected $personalizationIpService;
+
+/******************************************************************************
+ **                                                                          **
  ** This is an example of Dependency Injection. The necessary objects are    **
  ** being injected through the class's constructor.                          **
  **                                                                          **
@@ -45,10 +71,16 @@ class EcoActionBlock extends BlockBase implements ContainerFactoryPluginInterfac
    *   The plugin implementation definition.
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity type manager service.
+   * @param \Drupal\Core\Entity\EntityRepositoryInterface $entity_repository
+   *   The entity repository.
+   * @param \Drupal\iai_personalization\PersonalizationIpServiceInterface $personalization_ip_service
+   *   The personalization Ip Service.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, EntityRepositoryInterface $entity_repository, PersonalizationIpServiceInterface $personalization_ip_service) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->nodeStorage = $entity_type_manager->getStorage('node');
+    $this->entityRepository = $entity_repository;
+    $this->personalizationIpService = $personalization_ip_service;
   }
 
 /******************************************************************************
@@ -77,7 +109,9 @@ class EcoActionBlock extends BlockBase implements ContainerFactoryPluginInterfac
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('entity_type.manager')
+      $container->get('entity_type.manager'),
+      $container->get('entity.repository'),
+      $container->get('iai_personalization.personalization_ip_service')
     );
   }
 
@@ -88,6 +122,7 @@ class EcoActionBlock extends BlockBase implements ContainerFactoryPluginInterfac
     // By default, the block will contain 5 items and set the radius to 10.
     return array(
       'block_count' => 5,
+      'radius' => 10,
     );
   }
 
@@ -102,6 +137,12 @@ class EcoActionBlock extends BlockBase implements ContainerFactoryPluginInterfac
       '#default_value' => $this->configuration['block_count'],
       '#options' => array_combine($range, $range),
     );
+    $form['radius'] = array(
+      '#type' => 'select',
+      '#title' => $this->t('Radius (km) within which to search for action items.'),
+      '#default_value' => $this->configuration['radius'],
+      '#options' => array_combine($range, $range),
+    );
     return $form;
   }
 
@@ -110,6 +151,7 @@ class EcoActionBlock extends BlockBase implements ContainerFactoryPluginInterfac
    */
   public function blockSubmit($form, FormStateInterface $form_state) {
     $this->configuration['block_count'] = $form_state->getValue('block_count');
+    $this->configuration['radius'] = $form_state->getValue('radius');
   }
 
   /**
@@ -124,6 +166,9 @@ class EcoActionBlock extends BlockBase implements ContainerFactoryPluginInterfac
  ** certain radius of the user.                                              **
  **                                                                          **
  ******************************************************************************/
+    $radius = $this->configuration['radius'];
+    $ip = $this->personalizationIpService->getIpAddress();
+    $coordinates = $this->personalizationIpService->mapIpAddress($ip);
     $result = $this->nodeStorage->getQuery()
       ->condition('type', 'water_eco_action')
       ->condition('status', 1)
@@ -134,15 +179,20 @@ class EcoActionBlock extends BlockBase implements ContainerFactoryPluginInterfac
     if ($result) {
       $items = $this->nodeStorage->loadMultiple($result);
 
+      $build['ip_address_and_radius'] = [
+        '#type' => 'markup',
+        '#markup' => $this->t('Eco actions within @radius kilometers of @coordinates [@ipaddress]', array('@radius' => $radius, '@ipaddress' => $ip, '@coordinates' => $coordinates)),
+      ];
       $build['list'] = [
         '#theme' => 'item_list',
         '#items' => [],
       ];
       foreach ($items as $item) {
-        $url = Url::fromRoute('entity.node.canonical', array('node' => $item->nid->value));
+        $translatedItem = $this->entityRepository->getTranslationFromContext($item);
+        $url = Url::fromRoute('entity.node.canonical', array('node' => $translatedItem->nid->value));
         $build['list']['#items'][$item->id()] = [
           '#type' => 'markup',
-          '#markup' => Link::fromTextAndUrl($item->label(), $url)->toString(),
+          '#markup' => Link::fromTextAndUrl($translatedItem->label(), $url)->toString(),
         ];
       }
     }
