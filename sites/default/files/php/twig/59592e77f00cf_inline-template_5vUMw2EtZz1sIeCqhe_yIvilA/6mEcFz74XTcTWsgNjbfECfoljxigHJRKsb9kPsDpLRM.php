f62de1b853d0d21cc923a25777c768485202f963
Marc Isaacson <marc@isaacsonwebdevelopment.com>
2017-07-02 12:18:19 -0600

    Enable REST UI

---
+++ sites/default/files/php/twig/59592e77f00cf_inline-template_5vUMw2EtZz1sIeCqhe_yIvilA/6mEcFz74XTcTWsgNjbfECfoljxigHJRKsb9kPsDpLRM.php
@@ -0,0 +1,60 @@
+<?php
+
+/* {# inline_template_start #}<code>/entity/field_config/{field_config}</code>: GET */
+class __TwigTemplate_d647b4c4bb1283f19bb342a5de90e6a7bcfd25c9462a95786f0901876fbd09b3 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo "<code>/entity/field_config/{field_config}</code>: GET";
+    }
+
+    public function getTemplateName()
+    {
+        return "{# inline_template_start #}<code>/entity/field_config/{field_config}</code>: GET";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 1,);
+    }
+
+    public function getSource()
+    {
+        return "{# inline_template_start #}<code>/entity/field_config/{field_config}</code>: GET";
+    }
+}
---
+++ sites/default/files/php/twig/59592e77f00cf_inline-template_8pjkNIPToV67wXpVCbFUbPIps/Ur0-eXwImwdREVVcyb1j5Y9cJzuKupYjQtxyYuzbI1s.php
@@ -0,0 +1,60 @@
+<?php
+
+/* {# inline_template_start #}<code>/entity/node_type/{node_type}</code>: GET */
+class __TwigTemplate_617443f8ab4f3214af3b17bac4e417afcc83722edd8c0f40e72040a599e34ada extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo "<code>/entity/node_type/{node_type}</code>: GET";
+    }
+
+    public function getTemplateName()
+    {
+        return "{# inline_template_start #}<code>/entity/node_type/{node_type}</code>: GET";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 1,);
+    }
+
+    public function getSource()
+    {
+        return "{# inline_template_start #}<code>/entity/node_type/{node_type}</code>: GET";
+    }
+}
