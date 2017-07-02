1d763681500c80027eb29d1104fa80d3cc8f927c
Marc Isaacson <marc@isaacsonwebdevelopment.com>
2017-07-02 11:39:43 -0600

    Update to Drupal 8.3.4

---
+++ sites/default/files/php/twig/59592e77f00cf_datetime-form.html.twig_5I64KIfLh7MeqTfkmGjZnAvZk/z8NjXB6s7VkzeX-MC_LyxbcM6uwsXsj0cV4tmyAC6RM.php
@@ -0,0 +1,88 @@
+<?php
+
+/* core/themes/classy/templates/form/datetime-form.html.twig */
+class __TwigTemplate_f3c043353af582d926edc4f0c5826fc3ec8eca63855002ddac700485c2523c41 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 13
+        echo "<div";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->getAttribute((isset($context["attributes"]) ? $context["attributes"] : null), "addClass", array(0 => "container-inline"), "method"), "html", null, true));
+        echo ">
+  ";
+        // line 14
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["content"]) ? $context["content"] : null), "html", null, true));
+        echo "
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/classy/templates/form/datetime-form.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  48 => 14,  43 => 13,);
+    }
+
+    public function getSource()
+    {
+        return "{#
+/**
+ * @file
+ * Theme override of a datetime form element.
+ *
+ * Available variables:
+ * - attributes: HTML attributes for the datetime form element.
+ * - content: The datelist form element to be output.
+ *
+ * @see template_preprocess_datetime_form()
+ */
+#}
+<div{{ attributes.addClass('container-inline') }}>
+  {{ content }}
+</div>
+";
+    }
+}
