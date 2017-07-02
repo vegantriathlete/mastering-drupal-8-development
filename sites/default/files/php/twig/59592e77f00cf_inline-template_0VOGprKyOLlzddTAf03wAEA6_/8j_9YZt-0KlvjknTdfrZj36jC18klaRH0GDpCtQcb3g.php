a2d3102950da2027d257543a8dd12492485c0ab4
Marc Isaacson <marc@isaacsonwebdevelopment.com>
2017-07-02 12:18:19 -0600

    Enable REST UI

---
+++ sites/default/files/php/twig/59592e77f00cf_inline-template_0VOGprKyOLlzddTAf03wAEA6_/8j_9YZt-0KlvjknTdfrZj36jC18klaRH0GDpCtQcb3g.php
@@ -0,0 +1,60 @@
+<?php
+
+/* {# inline_template_start #}<code>/entity/taxonomy_vocabulary/{taxonomy_vocabulary}</code>: GET */
+class __TwigTemplate_92831dfce2682d5c4ef5f4e90811a2d556f56101fffe3b0be8444fa5398204d0 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo "<code>/entity/taxonomy_vocabulary/{taxonomy_vocabulary}</code>: GET";
+    }
+
+    public function getTemplateName()
+    {
+        return "{# inline_template_start #}<code>/entity/taxonomy_vocabulary/{taxonomy_vocabulary}</code>: GET";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 1,);
+    }
+
+    public function getSource()
+    {
+        return "{# inline_template_start #}<code>/entity/taxonomy_vocabulary/{taxonomy_vocabulary}</code>: GET";
+    }
+}
---
+++ sites/default/files/php/twig/59592e77f00cf_inline-template_bDfJ1guyEVH8p_Mwfp4gvOSlD/TfGJ4xMZxDV0A6vMhn0MIjklYpFJ1hj3okPIfnvGUJ0.php
@@ -0,0 +1,60 @@
+<?php
+
+/* {# inline_template_start #}<code>/entity/base_field_override/{base_field_override}</code>: GET */
+class __TwigTemplate_043f9ed5c4cf10c4b782b1edaf13eeb1ac9d1287028966c09dc713f2a785e8ef extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo "<code>/entity/base_field_override/{base_field_override}</code>: GET";
+    }
+
+    public function getTemplateName()
+    {
+        return "{# inline_template_start #}<code>/entity/base_field_override/{base_field_override}</code>: GET";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 1,);
+    }
+
+    public function getSource()
+    {
+        return "{# inline_template_start #}<code>/entity/base_field_override/{base_field_override}</code>: GET";
+    }
+}
