6207011a7572a704c36bea58129adf5eb00048f7
Marc Isaacson <marc@isaacsonwebdevelopment.com>
2017-07-02 12:18:19 -0600

    Enable REST UI

---
+++ sites/default/files/php/twig/59592e77f00cf_inline-template_XJT7cqHc6ST9QBmB-lH7anWZ9/JsBd7B1YMD3HN6WIIAT3WNfWzt9NfH40M5uKVvlASwI.php
@@ -0,0 +1,60 @@
+<?php
+
+/* {# inline_template_start #}<code>/entity/search_page/{search_page}</code>: GET */
+class __TwigTemplate_1c914303c9cd40d210b6d9bb70744f57474da7f998309513eac30141762ead53 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo "<code>/entity/search_page/{search_page}</code>: GET";
+    }
+
+    public function getTemplateName()
+    {
+        return "{# inline_template_start #}<code>/entity/search_page/{search_page}</code>: GET";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 1,);
+    }
+
+    public function getSource()
+    {
+        return "{# inline_template_start #}<code>/entity/search_page/{search_page}</code>: GET";
+    }
+}
